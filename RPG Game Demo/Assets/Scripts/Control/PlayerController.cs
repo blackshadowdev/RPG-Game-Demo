f1c4682da4221a6d14955a2d82f610dfca5085f4
Mert Oyman <mertoyman@gmail.com>
2019-08-18 01:34:12 +0300

    Separated the control layer

---
+++ RPG Game Demo/Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Mover mover;
+    RaycastHit hit;
+        
+    void Start()
+    {
+        mover = FindObjectOfType<Mover>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            CreateRaycast();
+            mover.MoveTo(hit.point);
+        }
+    }
+
+    void CreateRaycast(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit){
+            hit.point = hit.point;
+        }
+    }
+}
--- RPG Game Demo/Assets/Scripts/Movement/Mover.cs
+++ RPG Game Demo/Assets/Scripts/Movement/Mover.cs
@@ -12,19 +12,13 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0)){
-            MoveToCursor();
-        }
+        
         UpdateAnimator();
    }
 
-    void MoveToCursor(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if(hasHit){
-            agent.destination = hit.point; 
-        }
+    public void MoveTo(Vector3 destination)
+    {
+        agent.destination = destination;
     }
 
     //Set blend tree speed to speed of the navmesh agent
