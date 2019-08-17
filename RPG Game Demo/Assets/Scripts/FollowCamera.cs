e3b175f60f6db5748b5ca749f847815e196da454
Mert Oyman <mertoyman@gmail.com>
2019-08-17 20:18:41 +0300

    Created a fixed follow camera

---
+++ RPG Game Demo/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void Update()
+    {
+        transform.position = target.position;        
+    }
+}
