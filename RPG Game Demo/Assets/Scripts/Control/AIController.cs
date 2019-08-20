a8d28e293dcf9527c8a014f4a34f19a573830f6c
Mert Oyman <mertoyman@gmail.com>
2019-08-20 18:01:19 +0300

    Visualize chase distance with gizmos

--- RPG Game Demo/Assets/Scripts/Control/AIController.cs
+++ RPG Game Demo/Assets/Scripts/Control/AIController.cs
@@ -35,6 +35,11 @@ namespace RPG.Control
             return distanceToPlayer < chaseDistance;
         }
         
+        //Called by Unity
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
    }
 
 }
