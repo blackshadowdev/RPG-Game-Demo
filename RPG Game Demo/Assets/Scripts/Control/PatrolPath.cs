3912c0417691a76a1b623a5ae91e95416fbdace9
Mert Oyman <mertoyman@gmail.com>
2019-08-20 21:32:27 +0300

    Waypoint setup

---
+++ RPG Game Demo/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos() {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+
+        private int GetNextIndex(int i)
+        {
+            return (i + 1) % transform.childCount;
+        }
+    }    
+}
