ce57f754e7a14069a1f6615c8d31e74ad31647b6
Mert Oyman <mertoyman@gmail.com>
2019-09-10 17:44:11 +0300

    Fade is persisted

---
+++ RPG Game Demo/Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentObjectPrefab;
+        bool hasSpawned;
+
+        private void Awake() {
+            if(hasSpawned) return;
+            SpawnPersistentObjects();
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
