0541a09c1e66a1b7657dbfba708e5e92af232309
Mert Oyman <mertoyman@gmail.com>
2019-09-10 19:15:51 +0300

    Save and load player's position

--- RPG Game Demo/Assets/Scripts/Movement/Mover.cs
+++ RPG Game Demo/Assets/Scripts/Movement/Mover.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         private NavMeshAgent agent;
         Health health;
@@ -45,6 +46,19 @@ namespace RPG.Movement
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3) state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 
