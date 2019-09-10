6131887628a9f4c11ae0c1051e27fac15b306756
Mert Oyman <mertoyman@gmail.com>
2019-09-10 19:29:13 +0300

    Saveable health

--- RPG Game Demo/Assets/Scripts/Core/Health.cs
+++ RPG Game Demo/Assets/Scripts/Core/Health.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float health = 100f;
 
         bool isDead;
 
+
         public bool IsDead(){
             return isDead;
         }
+
 
         public void TakeDamage(float damage){
             if(isDead) return;
@@ -27,6 +30,21 @@ namespace RPG.Core
             isDead = true;
             GetComponent<Animator>().SetTrigger("die");
             GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+        
+        public object CaptureState()
+        {
+            return health;
+        }
+
+        public void RestoreState(object state)
+        {
+            health = (float)state;
+
+            if (health <= 0)
+            {
+                Die();
+            }
         }
     }    
 }
