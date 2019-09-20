aa3e3da28946f3004b4783a18f1057105e1d9d97
Mert Oyman <mertoyman@gmail.com>
2019-09-20 18:16:13 +0300

    Scriptable objects updated

--- RPG Game Demo/Assets/Scripts/Combat/Fighter.cs
+++ RPG Game Demo/Assets/Scripts/Combat/Fighter.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
-using System;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
-        [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
-        [SerializeField] float weaponDamage = 5f;        
         [SerializeField] Transform handTransform = null;
         [SerializeField] Weapon weapon = null;
        
@@ -65,7 +62,7 @@ namespace RPG.Combat
         //Animation event
         void Hit(){
             if(target == null) return;
-            target.TakeDamage(weaponDamage);
+            target.TakeDamage(weapon.GetDamage());
         }
 
         public bool CanAttack(GameObject combatTarget){
@@ -76,7 +73,7 @@ namespace RPG.Combat
 
         private bool IsInRange()
         {
-            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < weapon.GetRange();
         }
 
         public void Attack(GameObject combatTarget){
