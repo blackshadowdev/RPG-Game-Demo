6ca72b34467b0158fba0b513d51e3ff9518cc1ed
Mert Oyman <mertoyman@gmail.com>
2019-09-20 22:44:02 +0300

    Revert "Revert "Simple weapon pickup""
    This reverts commit bb7f1a4890acb9ec1864a9920dc213f4e5e0a849.

---
+++ RPG Game Demo/Assets/Scripts/Combat/WeaponPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using RPG.Combat;
+
+namespace RPG.Combat
+{
+    public class WeaponPickup : MonoBehaviour
+    {
+        [SerializeField] Weapon weapon = null;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                other.GetComponent<Fighter>().EquipWeapon(weapon);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
