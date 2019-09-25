f0c2d2cec7ed02ae703a9bfdf774d5ae135eb53d
Mert Oyman <mertoyman@gmail.com>
2019-09-25 16:13:00 +0300

    Editing scriptable object files

--- RPG Game Demo/Assets/Scripts/Stats/Progression.cs
+++ RPG Game Demo/Assets/Scripts/Stats/Progression.cs
@@ -10,7 +10,8 @@ namespace RPG.Stats
         public float GetHealth(CharacterClass characterClass, int level){
             foreach(ProgressionCharacterClass progressionClass in characterClasses){
                 if (progressionClass.characterClass == characterClass){
-                    return progressionClass.health[level - 1];
+            //        return progressionClass.health[level - 1];
+                        
                 }
             }
             return 0;
@@ -18,8 +19,14 @@ namespace RPG.Stats
         
         [System.Serializable]
         class ProgressionCharacterClass{
-            public CharacterClass characterClass;
-            public float[] health;
+            public CharacterClass characterClass;           
+            public ProgressionStat[] stats;
+        }
+
+        [System.Serializable]
+        class ProgressionStat {
+            public Stat stat;
+            public float[] levels;
         }
     }    
 }
